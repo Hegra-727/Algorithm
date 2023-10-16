3e8c1dafad42cc97347b2194b7ac659d42baacc7
Hegra-727 <Hegra727@outlook.com>
2023-10-16 23:22:05 +0800

    哈希map

--- LeetCode/src/Daily/Oct2023/只出现一次的数字II137.java
+++ LeetCode/src/Daily/Oct2023/只出现一次的数字II137.java
@@ -11,7 +11,7 @@ public class 只出现一次的数字II137 {
     static int singleNumber(int[] nums) {
         HashMap<Integer,Integer> hashMap=new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            hashMap.put(i, hashMap.getOrDefault(i,0)+1);
+            hashMap.put(nums[i], hashMap.getOrDefault(nums[i],0)+1);
         }
         int ans=0;
         for(Map.Entry<Integer,Integer> entry: hashMap.entrySet()){
