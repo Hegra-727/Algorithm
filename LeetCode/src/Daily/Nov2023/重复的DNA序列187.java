fad72239199d34cabdefefd4f7ad7ff8e2b01ca9
Hegra-727 <Hegra727@outlook.com>
2023-11-05 23:00:28 +0800

    哈希表枚举子串

---
+++ LeetCode/src/Daily/Nov2023/重复的DNA序列187.java
@@ -0,0 +1,24 @@
+package Daily.Nov2023;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class 重复的DNA序列187 {
+    static final int L = 10;
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> ans = new ArrayList<String>();
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        int n = s.length();
+        for (int i = 0; i <= n - L; ++i) {
+            String sub = s.substring(i, i + L);
+            cnt.put(sub, cnt.getOrDefault(sub, 0) + 1);
+            if (cnt.get(sub) == 2) {
+                ans.add(sub);
+            }
+        }
+        return ans;
+    }
+}
